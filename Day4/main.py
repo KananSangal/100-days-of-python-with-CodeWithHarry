c803f5701fe560ce35db515610db8f5f65a932c6
KananSangal <kanansangal@gmail.com>
2026-05-24 13:30:14 +0530

    Add Day 4 code demonstrating basic print statements and a multi-line string quiz

---
+++ Day4/main.py
@@ -0,0 +1,12 @@
+print("Hello World", 7)
+print(5)
+print("Bye")
+print(17*13)
+
+# Solving the Quick Quiz using triple quotes for multi-line text
+print("""
+Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+""")
