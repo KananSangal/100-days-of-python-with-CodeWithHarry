2163b9954bedec8528f306465353be3b9e58976b
KananSangal <kanansangal@gmail.com>
2026-05-26 23:49:27 +0530

    Add Day 7 code for basic calculator demonstrating arithmetic operations in Python

---
+++ Day7/main.py
@@ -0,0 +1,17 @@
+print(" Kanan's Calculator")
+a = 10
+b = 3
+sum = a+b
+print ("The addition of a and b is : " , sum)
+difference = a-b
+print ("The subtraction of a and b is : " , difference)
+product = a*b
+print ("The multiplication of a and b is : " , product)
+quotient = a/b
+print ("The division of a and b is : " , quotient)
+exponentiation = a**b
+print ("The exponentiation of a and b is : " , exponentiation)
+modulus = a%b
+print ("The modulus or remainder of a and b is : " , modulus)
+floor_division = a//b
+print ("The floor/integer division of a and b is : " , floor_division)
