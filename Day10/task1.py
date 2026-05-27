df8bf80fa26418e8e6d0a177e792c1800fbae684
KananSangal <kanansangal@gmail.com>
2026-05-27 12:52:05 +0530

    Add Day 10 code demonstrating arithmetic operations with user input integers in Python

---
+++ Day10/task1.py
@@ -0,0 +1,30 @@
+x=input("Enter First No: ") #50
+y=input("Enter Second No: ") #3
+
+print("Addition:")
+print(x+y) #503
+print(int(x) + int(y)) #53
+
+print("Subtraction:")
+# print(x-y) Not possible because x and y are strings
+print(int(x) - int(y)) #47
+
+print("Multiplication:")
+#print(x*y) Not possible because x and y are strings.
+print(int(x) * int(y)) #150
+
+print("Division:")
+# print(x/y) Not possible because x and y are strings.
+print(int(x) / int(y)) #16.666666666666668
+
+print("Floor Division:")
+# print(x//y) Not possible because x and y are strings.
+print(int(x) // int(y)) #16
+
+print("Modulus:")
+# print(x%y) Not possible because x and y are strings.
+print(int(x) % int(y)) #2
+
+print("Exponentiation:")
+# print(x**y) Not possible because x and y are strings.
+print(int(x) ** int(y)) #125000
